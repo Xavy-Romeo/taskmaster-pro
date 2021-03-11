79a7e13a191b3ff7f20a02df556d014b28f6a6f3
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-03-11 12:28:39 -0800

    added drag and drop capabilities to all cards and remove section

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,20 +44,60 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// modal was triggered
+$("#task-form-modal").on("show.bs.modal", function() {
+  // clear values
+  $("#modalTaskDescription, #modalDueDate").val("");
+});
+
+// modal is fully visible
+$("#task-form-modal").on("shown.bs.modal", function() {
+  // highlight textarea
+  $("#modalTaskDescription").trigger("focus");
+});
+
+// save button in modal was clicked
+$("#task-form-modal .btn-primary").click(function() {
+  // get form values
+  var taskText = $("#modalTaskDescription").val();
+  var taskDate = $("#modalDueDate").val();
+
+  if (taskText && taskDate) {
+    createTask(taskText, taskDate, "toDo");
+
+    // close modal
+    $("#task-form-modal").modal("hide");
+
+    // save in tasks array
+    tasks.toDo.push({
+      text: taskText,
+      date: taskDate
+    });
+
+    saveTasks();
+  }
+});
+
+// task text was clicked
 $(".list-group").on("click", "p", function(){
   
+  // get current text of p element
   var text = $(this)
     .text()
     .trim();
 
+  // replace p element with a new textarea  
   var textInput = $("<textarea>")
     .addClass("form-control")
     .val(text);
 
   $(this).replaceWith(textInput);
+
+  // auto focus new Input element
   textInput.trigger("focus");
 });
 
+// unfocus editable field
 $(".list-group").on("blur", "textarea", function() {
 
   // get the textarea's current value/text
@@ -76,6 +116,7 @@ $(".list-group").on("blur", "textarea", function() {
     .closest(".list-group-item")
     .index();
 
+  // update task in array and re-save to localStorage  
   tasks[status][index].text = text;
   saveTasks();
 
@@ -141,41 +182,6 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
-
-// modal was triggered
-$("#task-form-modal").on("show.bs.modal", function() {
-  // clear values
-  $("#modalTaskDescription, #modalDueDate").val("");
-});
-
-// modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function() {
-  // highlight textarea
-  $("#modalTaskDescription").trigger("focus");
-});
-
-// save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
-  // get form values
-  var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val();
-
-  if (taskText && taskDate) {
-    createTask(taskText, taskDate, "toDo");
-
-    // close modal
-    $("#task-form-modal").modal("hide");
-
-    // save in tasks array
-    tasks.toDo.push({
-      text: taskText,
-      date: taskDate
-    });
-
-    saveTasks();
-  }
-});
-
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -188,4 +194,77 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  
+  update: function(event) {
+    
+    // array to store the task data in
+    var tempArr = [];
+    
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find('p')
+        .text()
+        .trim();
 
+      var date = $(this)
+        .find('span')
+        .text()
+        .trim();
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "")
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  },
+});
+
+$('#trash').droppable({
+
+  accept: '.card .list-group-item',
+  tolerance: 'touch',
+  
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log('drop');
+  },
+
+  over: function(event,ui) {
+    console.log('over');
+  },
+
+  out: function(event,ui) {
+    console.log('out');
+  },
+});
